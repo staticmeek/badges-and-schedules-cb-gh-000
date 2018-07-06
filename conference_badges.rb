2fc082635d0652d4c22dae436765ca8075963d2e
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 02:45:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
 # Write your code here.
+["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  
+badge_message
 end
 
 def assign_rooms(name)
