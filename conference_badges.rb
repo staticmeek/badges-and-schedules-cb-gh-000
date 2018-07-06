5cb5824e38544938ec8bc4819706c88357ca3799
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 02:45:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
