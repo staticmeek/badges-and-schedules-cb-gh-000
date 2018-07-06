e9ccb154eee15793263251c83e5d8fc1b0f2788f
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 02:42:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = []
+  while badge_maker != nil
+    attendees << [badge_maker(name)]
+  end
+end
+
+def assign_rooms(name)
+
+end
+
+def printer (attendees)
+
+end
