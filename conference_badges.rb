6c59873eb2d8c073e55c46c8a4baf925c411080d
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 02:50:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)  
-
+def batch_badge_creator(name)
+  count = 0
+  while count <= attendees.length-1
+    badges = [badge_maker(name)]
+    count += 1
+  end
 end
-
