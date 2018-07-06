81456315cf7212e9c1e50738ece64c5ad265ab0c
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 03:11:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,7 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def badge_maker(name)
-  name = []
-  name << "Hello, my name is #{name}"
+def badge_maker(name)  
+ "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(name)
