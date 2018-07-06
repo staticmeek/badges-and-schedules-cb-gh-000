72656de77d0336184e5ead2b6f56797773c81440
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 03:28:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+require 'pry'
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -6,5 +7,4 @@ end
 
 def batch_badge_creator(attendees)
   counter = name.size
-  while    
 end
