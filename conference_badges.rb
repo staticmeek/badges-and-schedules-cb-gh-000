5aa546f852bc233c803a888b0056eef490cb641a
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 02:42:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees = []
-  while badge_maker != nil
-    attendees << [badge_maker(name)]
-  end
+  
 end
 
 def assign_rooms(name)
